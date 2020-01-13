6b8c50055476c24bd759d289df0e466fec7eaa9c
CosminConstantin <cosmintarnauceanu98@gmail.com>
2020-01-14 00:12:01 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,367 @@
+﻿namespace IA_C
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
+            this.button21 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button22 = new System.Windows.Forms.Button();
+            this.button23 = new System.Windows.Forms.Button();
+            this.button24 = new System.Windows.Forms.Button();
+            this.button25 = new System.Windows.Forms.Button();
+            this.button26 = new System.Windows.Forms.Button();
+            this.agent = new System.Windows.Forms.Button();
+            this.CurrentScore = new System.Windows.Forms.Label();
+            this.scoreValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 50);
+            this.button2.TabIndex = 0;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(50, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 50);
+            this.button3.TabIndex = 1;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(100, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 50);
+            this.button4.TabIndex = 2;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(150, 0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 50);
+            this.button5.TabIndex = 3;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.LawnGreen;
+            this.button6.Location = new System.Drawing.Point(200, 0);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 50);
+            this.button6.TabIndex = 4;
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Red;
+            this.button7.Location = new System.Drawing.Point(200, 50);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 50);
+            this.button7.TabIndex = 9;
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(150, 50);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(50, 50);
+            this.button8.TabIndex = 8;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button9.Location = new System.Drawing.Point(100, 50);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(50, 50);
+            this.button9.TabIndex = 7;
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button10.Location = new System.Drawing.Point(50, 50);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(50, 50);
+            this.button10.TabIndex = 6;
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(0, 50);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(50, 50);
+            this.button11.TabIndex = 5;
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(200, 100);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(50, 50);
+            this.button17.TabIndex = 14;
+            this.button17.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(150, 100);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(50, 50);
+            this.button18.TabIndex = 13;
+            this.button18.UseVisualStyleBackColor = true;
+            // 
+            // button19
+            // 
+            this.button19.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button19.Location = new System.Drawing.Point(100, 100);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(50, 50);
+            this.button19.TabIndex = 12;
+            this.button19.UseVisualStyleBackColor = false;
+            // 
+            // button20
+            // 
+            this.button20.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button20.Location = new System.Drawing.Point(50, 100);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(50, 50);
+            this.button20.TabIndex = 11;
+            this.button20.UseVisualStyleBackColor = false;
+            // 
+            // button21
+            // 
+            this.button21.Location = new System.Drawing.Point(0, 100);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(50, 50);
+            this.button21.TabIndex = 10;
+            this.button21.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(200, 150);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(50, 50);
+            this.button12.TabIndex = 19;
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(150, 150);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(50, 50);
+            this.button13.TabIndex = 18;
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(100, 150);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(50, 50);
+            this.button14.TabIndex = 17;
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(50, 150);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(50, 50);
+            this.button15.TabIndex = 16;
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(0, 150);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(50, 50);
+            this.button16.TabIndex = 15;
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // button22
+            // 
+            this.button22.Location = new System.Drawing.Point(200, 200);
+            this.button22.Name = "button22";
+            this.button22.Size = new System.Drawing.Size(50, 50);
+            this.button22.TabIndex = 24;
+            this.button22.UseVisualStyleBackColor = true;
+            // 
+            // button23
+            // 
+            this.button23.Location = new System.Drawing.Point(150, 200);
+            this.button23.Name = "button23";
+            this.button23.Size = new System.Drawing.Size(50, 50);
+            this.button23.TabIndex = 23;
+            this.button23.UseVisualStyleBackColor = true;
+            // 
+            // button24
+            // 
+            this.button24.Location = new System.Drawing.Point(100, 200);
+            this.button24.Name = "button24";
+            this.button24.Size = new System.Drawing.Size(50, 50);
+            this.button24.TabIndex = 22;
+            this.button24.UseVisualStyleBackColor = true;
+            // 
+            // button25
+            // 
+            this.button25.Location = new System.Drawing.Point(50, 200);
+            this.button25.Name = "button25";
+            this.button25.Size = new System.Drawing.Size(50, 50);
+            this.button25.TabIndex = 21;
+            this.button25.UseVisualStyleBackColor = true;
+            // 
+            // button26
+            // 
+            this.button26.Location = new System.Drawing.Point(0, 200);
+            this.button26.Name = "button26";
+            this.button26.Size = new System.Drawing.Size(50, 50);
+            this.button26.TabIndex = 20;
+            this.button26.UseVisualStyleBackColor = true;
+            // 
+            // agent
+            // 
+            this.agent.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.agent.Location = new System.Drawing.Point(0, 200);
+            this.agent.Name = "agent";
+            this.agent.Size = new System.Drawing.Size(50, 50);
+            this.agent.TabIndex = 25;
+            this.agent.Text = "agent";
+            this.agent.UseVisualStyleBackColor = false;
+            // 
+            // CurrentScore
+            // 
+            this.CurrentScore.AutoSize = true;
+            this.CurrentScore.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.CurrentScore.Location = new System.Drawing.Point(257, 9);
+            this.CurrentScore.Name = "CurrentScore";
+            this.CurrentScore.Size = new System.Drawing.Size(35, 13);
+            this.CurrentScore.TabIndex = 26;
+            this.CurrentScore.Text = "Score";
+            // 
+            // scoreValue
+            // 
+            this.scoreValue.AutoSize = true;
+            this.scoreValue.Location = new System.Drawing.Point(257, 37);
+            this.scoreValue.Name = "scoreValue";
+            this.scoreValue.Size = new System.Drawing.Size(13, 13);
+            this.scoreValue.TabIndex = 27;
+            this.scoreValue.Text = "1";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(325, 253);
+            this.Controls.Add(this.scoreValue);
+            this.Controls.Add(this.CurrentScore);
+            this.Controls.Add(this.agent);
+            this.Controls.Add(this.button22);
+            this.Controls.Add(this.button23);
+            this.Controls.Add(this.button24);
+            this.Controls.Add(this.button25);
+            this.Controls.Add(this.button26);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button20);
+            this.Controls.Add(this.button21);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.onClose);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button22;
+        private System.Windows.Forms.Button button23;
+        private System.Windows.Forms.Button button24;
+        private System.Windows.Forms.Button button25;
+        private System.Windows.Forms.Button button26;
+        private System.Windows.Forms.Button agent;
+        private System.Windows.Forms.Label CurrentScore;
+        private System.Windows.Forms.Label scoreValue;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+
+namespace IA_C
+{
+    public partial class Form1 : Form
+    {
+        class EnableDirection
+        {
+            public bool goUp;
+            public bool goDown;
+            public bool goLeft;
+            public bool goRight;
+
+            public EnableDirection(bool up, bool down, bool left, bool right)
+            {
+                goUp = up;
+                goDown = down;
+                goLeft = left;
+                goRight = right;
+            }
+        };
+        class Direction
+        {
+            public double up;
+            public double down;
+            public double left;
+            public double right;
+            public EnableDirection enabled;
+            public Direction(double mup, double mdown, double mleft, double mright, bool eup, bool edown, bool eleft, bool eright)
+            {
+                enabled = new EnableDirection(eup, edown, eleft, eright);
+                up = mup;
+                down = mdown;
+                left = mleft;
+                right = mright;
+            }
+        };
+
+        int startX = 4, startY = 0;
+        int greenX = 0, greenY = 4;
+        int redX = 1, redY = 4;
+        int x, y;
+        double score = 1;
+        Direction[,] Q = new Direction[5, 5];
+        double alpha = 1;
+        double reward = -0.04;
+        bool restart = false;
+        double discount = 0.3;
+        Thread st;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ThreadStart thrS = new ThreadStart(run);
+            st = new Thread(thrS);
+            st.Start();
+        }
+
+        private void onClose(object sender, FormClosingEventArgs e)
+        {
+            st.Abort();
+        }
+        void initQ()
+        {
+            x = startX;
+            y = startY;
+            Q[0, 0] = new Direction(1, 1, 1, 1, false, true, false, true);
+            Q[0, 1] = new Direction(1, 1, 1, 1, false, false, true, true);
+            Q[0, 2] = new Direction(1, 1, 1, 1, false, false, true, true);
+            Q[0, 3] = new Direction(1, 1, 1, 1, false, true, true, true);
+            Q[0, 4] = new Direction(1, 1, 1, 1, false, false, false, false);
+
+            Q[1, 0] = new Direction(1, 1, 1, 1, true, true, false, false);
+            Q[1, 1] = new Direction(1, 1, 1, 1, false, false, false, false);
+            Q[1, 2] = new Direction(1, 1, 1, 1, false, false, false, false);
+            Q[1, 3] = new Direction(1, 1, 1, 1, true, true, false, true);
+            Q[1, 4] = new Direction(1, 1, 1, 1, false, false, false, false);
+
+            Q[2, 0] = new Direction(1, 1, 1, 1, true, true, false, false);
+            Q[2, 1] = new Direction(1, 1, 1, 1, false, false, false, false);
+            Q[2, 2] = new Direction(1, 1, 1, 1, false, false, false, false);
+            Q[2, 3] = new Direction(1, 1, 1, 1, true, true, false, true);
+            Q[2, 4] = new Direction(1, 1, 1, 1, true, true, true, false);
+
+            Q[3, 0] = new Direction(1, 1, 1, 1, true, true, false, true);
+            Q[3, 1] = new Direction(1, 1, 1, 1, false, true, true, true);
+            Q[3, 2] = new Direction(1, 1, 1, 1, false, true, true, true);
+            Q[3, 3] = new Direction(1, 1, 1, 1, true, true, true, true);
+            Q[3, 4] = new Direction(1, 1, 1, 1, true, true, true, false);
+
+            Q[4, 0] = new Direction(1, 1, 1, 1, true, false, false, true);
+            Q[4, 1] = new Direction(1, 1, 1, 1, true, false, true, true);
+            Q[4, 2] = new Direction(1, 1, 1, 1, true, false, true, true);
+            Q[4, 3] = new Direction(1, 1, 1, 1, true, false, true, true);
+            Q[4, 4] = new Direction(1, 1, 1, 1, true, false, true, false);
+        }
+
+        public void getMaxAction(ref double val, ref int action)
+        {
+            val = Q[x, y].enabled.goUp ? Q[x, y].up : (Q[x, y].enabled.goDown ? Q[x, y].down : (Q[x, y].enabled.goLeft ? Q[x, y].left : Q[x, y].right));
+            action = Q[x, y].enabled.goUp ? 0 : (Q[x, y].enabled.goDown ? 1 : (Q[x, y].enabled.goLeft ? 2 : 3));
+            if (Q[x, y].enabled.goUp == true)
+            {
+                if (val < Q[x, y].up)
+                {
+                    val = Q[x, y].up;
+                    action = 0;
+                }
+            }
+            if (Q[x, y].enabled.goDown == true)
+            {
+                if (val < Q[x, y].down)
+                {
+                    val = Q[x, y].down;
+                    action = 1;
+                }
+            }
+            if (Q[x, y].enabled.goLeft == true)
+            {
+                if (val < Q[x, y].left)
+                {
+                    val = Q[x, y].left;
+                    action = 2;
+                }
+            }
+            if (Q[x, y].enabled.goRight == true)
+            {
+                if (val < Q[x, y].right)
+                {
+                    val = Q[x, y].right;
+                    action = 3;
+                }
+            }
+        }
+
+        public void tryMove(int dx, int dy)
+        {
+            if (dx == -1 && Q[x, y].enabled.goUp == true)
+            {
+                x--;
+                agent.Top -= 50;
+            }
+            if (dx == 1 && Q[x, y].enabled.goDown == true)
+            {
+                x++;
+                agent.Top += 50;
+            }
+            if (dy == -1 && Q[x, y].enabled.goLeft == true)
+            {
+                y--;
+                agent.Left -= 50;
+            }
+            if (dy == 1 && Q[x, y].enabled.goRight == true)
+            {
+                y++;
+                agent.Left += 50;
+            }
+            score += reward;
+            if (y == greenY && x == greenX)
+            {
+                score += 1.04;
+                reward = 1;
+                restart = true;
+                if (score > 0)
+                {
+                    Console.WriteLine("Success - score: " + score.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("Fail - score: " + score.ToString());
+                }
+            }
+            if (y == redY && x == redX)
+            {
+                score -= 0.96;
+                reward = -1;
+                restart = true;
+                if (score > 0)
+                {
+                    Console.WriteLine("Success - score: " + score.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("Fail - score: " + score.ToString());
+                }
+            }
+            scoreValue.Text = score.ToString();
+        }
+
+
+        public void doMove(int action)
+        {
+            switch (action)
+            {
+                case 0:
+                    tryMove(-1, 0);
+                    break;
+                case 1:
+                    tryMove(+1, 0);
+                    break;
+                case 2:
+                    tryMove(0, -1);
+                    break;
+                case 3:
+                    tryMove(0, 1);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void modifyLastStateActionValue(int firstAction, double val)
+        {
+            int lastX, lastY;
+            switch (firstAction)
+            {
+                case 0:
+                    lastX = x + 1;
+                    Q[lastX, y].up = Q[lastX, y].up * (1.0 - alpha) + alpha * (reward + discount * val);
+                    break;
+                case 1:
+                    lastX = x - 1;
+                    Q[lastX, y].down = Q[lastX, y].down * (1.0 - alpha) + alpha * (reward + discount * val);
+                    break;
+                case 2:
+                    lastY = y + 1;
+                    Q[x, lastY].left = Q[x, lastY].left * (1.0 - alpha) + alpha * (reward + discount * val);
+                    break;
+                case 3:
+                    lastY = y - 1;
+                    Q[x, lastY].right = Q[x, lastY].right * (1.0 - alpha) + alpha * (reward + discount * val);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        public void run()
+        {
+            int firstAction = 0;
+            int secondAction = 0;
+            double maxVal = 0;
+            double t = 1;
+
+            initQ();
+
+            while (true)
+            {
+                //maxVal = -100;
+                getMaxAction(ref maxVal, ref firstAction);
+
+                //Console.WriteLine(maxVal + " " + firstAction);
+                //Console.WriteLine("Q " + x +" " + y +  "" + Q[x, y].up + " " + Q[x, y].down + " " + Q[x, y].left + " " + Q[x, y].right + " " + " " + alpha);
+
+                doMove(firstAction);
+
+                //maxVal = -100;
+                getMaxAction(ref maxVal, ref secondAction);
+                //Console.WriteLine(maxVal + " " + secondAction);
+                //Console.WriteLine("");
+                modifyLastStateActionValue(firstAction, maxVal);
+
+                t++;
+
+                if (restart == true)
+                {
+                    reward = -0.04;
+                    t = 1;
+                    score = 1;
+                    restart = false;
+                    x = startX;
+                    y = startY;
+                    agent.Location = new Point(0, 200);
+                }
+
+                alpha = Math.Pow(t, -0.1);
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
